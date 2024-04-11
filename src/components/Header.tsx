1c87b27ef2a0e90610ba2e50c15834dfabe0eda5
Sourav Ojha <subhamojha9@gmail.com>
2024-04-11 13:09:44 +0530

    minor fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,57 @@
-import { useReactFlow } from "reactflow";
+import { Edge, Node, useReactFlow } from "reactflow";
 import { setToLS } from "../utils";
 
 const Header = () => {
   const flow = useReactFlow();
 
+  const checkValidFlow = (nodes: Node[], edges: Edge[]) => {
+    // check for any node has 0 edges
+    const edgeCount: Record<string, number> = {};
+    let isValid = true;
+
+    edges.forEach((edge) => {
+      if (!edgeCount[edge.source]) {
+        edgeCount[edge.source] = 0;
+      }
+      if (!edgeCount[edge.target]) {
+        edgeCount[edge.target] = 0;
+      }
+      edgeCount[edge.source] += 1;
+      edgeCount[edge.target] += 1;
+    });
+
+    nodes.forEach((node) => {
+      if (edgeCount[node.id] === 0 || !edgeCount[node.id]) {
+        isValid = false;
+        return;
+      }
+    });
+
+    return isValid ? true : false;
+  };
+
   const handleSave = () => {
-    setToLS("edges", flow.getEdges());
-    setToLS("nodes", flow.getNodes());
+    const nodes = flow.getNodes();
+    const edges = flow.getEdges();
+
+    const isValid = checkValidFlow(nodes, edges);
+
+    if (!isValid) {
+      alert("Cannot Save. Some Node has no connections");
+    } else {
+      setToLS("nodes", nodes);
+      setToLS("edges", edges);
+      alert("Changes Saved");
+    }
   };
 
   return (
     <div className="w-full  bg-[#f3f3f3] ">
-      <div className=" mx-4  h-[6vh] flex items-center justify-end">
+      <div className=" mx-4  h-[6vh] flex items-center justify-between">
+        <div></div>
+        <div className="text-2xl font-semibold tracking-wide ">
+          Chatbot flow builder | BiteSpeed
+        </div>
         <button
           className="border-blue-500 border text-gray-700 bg-white rounded-md px-4 py-2 w-40 "
           onClick={handleSave}
--- src/components/NodeSetting.tsx
+++ src/components/NodeSetting.tsx
@@ -1,38 +1,52 @@
 import { useReactFlow } from "reactflow";
-import { useStore as useCustomStore } from "../store";
+import { useStore } from "../store";
+import { useEffect, useState } from "react";
 
 const NodeSetting = () => {
-  const store = useCustomStore((state) => state);
+  const selectedNode = useStore((state) => state.selectedNode);
+  const [input, setInput] = useState("");
 
   const flow = useReactFlow();
 
-  // update the label of the selected node
-  const handleUpdate = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const value = e.target.value;
-    flow.setNodes((nodes) =>
-      nodes.map((node) => {
-        if (node.id === store.selectedNode?.id) {
-          node.data = {
-            ...store.selectedNode.data,
-            label: value,
-          };
-        }
-
-        return node;
-      })
-    );
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(e.target.value);
   };
+
+  useEffect(() => {
+    // update the label of the selected node
+    const handleUpdate = () => {
+      flow.setNodes((nodes) =>
+        nodes.map((node) => {
+          if (node.id === selectedNode?.id) {
+            node.data = {
+              ...selectedNode.data,
+              label: input,
+            };
+          }
+
+          return node;
+        })
+      );
+    };
+    handleUpdate();
+  }, [input]);
+
+  useEffect(() => {
+    if (selectedNode) {
+      setInput(selectedNode.data.label);
+    }
+  }, [selectedNode]);
 
   return (
     <div className="min-w-96  h-full bg-white border-2  py-4  ">
       <div className="text-center  pb-1 px-4 ">Message</div>
       <hr className="my-2  " />
-      {store.selectedNode && (
+      {selectedNode && (
         <div className="p-4">
           <div className="text-gray-500 pb-2">Text</div>
           <textarea
-            defaultValue={store.selectedNode.data.label}
-            onChange={handleUpdate}
+            value={input}
+            onChange={handleInputChange}
             rows={4}
             className="border rounded-md border-gray-300 px-2 py-1 w-full"
           />
